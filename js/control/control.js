5e4266071fe9c0c56497d978840ff14f7780ba28
Maelström <Vous@exemple.com>
2022-03-31 09:17:06 +0200

    none

--- js/control/control.js
+++ js/control/control.js
@@ -1,7 +1,14 @@
+import {mine} from "../model/mine"
+import {view} from "../view/view"
+
 class control{
 
-constructor(){
+#view
+#model
 
+constructor(){
+	this.#view = new view();
+	this.#model = new mine();
 }
 
 }
--- js/model/model.js
+++ js/model/model.js
@@ -1,10 +1,10 @@
-import {view} from "../view/view.js"
+
 class model{
 
 #view
 
 constructor(){
-	this.#view = new view();
+	
 }
 
 }
